2cd93c3ab038de2fa34277ee861870f743e827ba
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-07-11 17:39:53 +0200

    integrate adminLTE, nav, footer and slidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,21 @@
 @extends('layouts.app')
 
+@section('title', 'Home')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Home Page</h1>
                 </div>
             </div>
         </div>
     </div>
-</div>
+
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Content here -->
+        </div>
+    </section>
 @endsection
---
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -0,0 +1,26 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+        <img src="{{ asset('Laravel.png') }}" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Menú principal</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <li class="nav-item">
+                    <a href="{{ route('home') }}" class="nav-link">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>Home</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                        <i class="nav-icon fas fa-users"></i>
+                        <p>Users</p>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</aside>
